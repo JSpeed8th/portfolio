719bd91d6248359de1ea170314145d30fb483d60
Jordan <jspeed8th@gmail.com>
2019-01-14 13:31:02 -0500

    Begun factoring code in views' js files

---
+++ src/contact/contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./contact.css";
+
+const contact = () => {
+  return <div />;
+};
+
+export default contact;
---
+++ src/projects/projects.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./projects.css";
+
+const projects = () => {
+  return <div />;
+};
+
+export default projects;
