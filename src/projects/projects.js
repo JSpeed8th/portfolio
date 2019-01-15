2ba3c7647104532775b189bf2b31fd20395833c8
Jordan <jspeed8th@gmail.com>
2019-01-15 14:54:21 -0500

    almost completed styling projects view

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -5,12 +5,39 @@ import backgroundimage from "../images/projects.jpg";
 import bakeryRecording from "../screenRecording/jordansBakery.mov";
 
 const projects = props => {
-  const videos = [{ path: bakeryRecording }];
+  const videos = [
+    {
+      path: bakeryRecording,
+      github: "https://github.com/JSpeed8th/portfolio",
+      livelink: "#",
+      title: "Jordan's Bakery & Company",
+      description:
+        'An application for, "jordan\'s bakery & company", with features including user sign-in, crud functionality, and the capability to order baked-goods.'
+    }
+  ];
   const videoArray = videos.map((video, index) => {
+    {
+      video.key = index;
+    }
     return (
-      <video autoPlay muted controls key={index}>
-        <source src={video.path} type="video/mp4" />
-      </video>
+      <div className="individual-project">
+        <div className="individual-project__left">
+          <video autoPlay muted replay="true" controls>
+            <source src={video.path} type="video/mp4" />
+          </video>
+          <div className="links">
+            <p className="project-title">{video.title}</p>
+            <a href={video.github} target="_blank">
+              GitHub
+            </a>
+            <span>/</span>
+            <a href={video.livelink}>Live Link </a>
+          </div>
+        </div>
+        <div className="individual-project__right">
+          <p>{video.description}</p>
+        </div>
+      </div>
     );
   });
 
