dd8cbe683bb809597efd5ce1ce4076384e1edbe8
Jordan <jspeed8th@gmail.com>
2019-01-15 13:30:35 -0500

    completed functionings for projects view.

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./projects.css";
 import "../App.css";
 import backgroundimage from "../images/projects.jpg";
+import bakeryRecording from "../screenRecording/jordansBakery.mov";
 
 const projects = props => {
+  const videos = [{ path: bakeryRecording }];
+  const videoArray = videos.map((video, index) => {
+    return (
+      <video autoplay muted controls key={index}>
+        <source src={video.path} type="video/mp4" />
+      </video>
+    );
+  });
   return (
     <div className="projects view">
       <img className="background-image" src={backgroundimage} />
       <h1 className="page-title">Projects</h1>
-      <div className="project-container" />
+      <div className="project-container">{videoArray}</div>
     </div>
   );
 };
