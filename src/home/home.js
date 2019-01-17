72cd77a4576a35fd57bd16b12de1211801e78bcf
Jordan <jspeed8th@gmail.com>
2019-01-17 14:12:06 -0500

    additional styling

--- src/home/home.js
+++ src/home/home.js
@@ -2,17 +2,17 @@ import React from "react";
 import SocialMedia from "../socialmedia/socialmedia";
 import "./home.css";
 import "../App.css";
-import backgroundimage from "../images/home.jpg";
 
 const home = props => {
   return (
     <div className="home-view">
       <SocialMedia />
-      <img className="home-image" src={backgroundimage} />
-      <h1 className="page-title">Home</h1>
-      <div className="home-title">
-        <h1>Jordan Speed</h1>
-        <p>full stack web developer</p>
+      <div className="home-image">
+        <h1 className="page-title">Home</h1>
+        <div className="home-title">
+          <h1>Jordan Speed</h1>
+          <p>full stack web developer</p>
+        </div>
       </div>
     </div>
   );
