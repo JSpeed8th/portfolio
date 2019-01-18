9277b6371c555534c5f6b61bb78290a3783a05d2
Jordan <jspeed8th@gmail.com>
2019-01-18 13:05:53 -0500

    ...

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialMedia from "../socialmedia/socialmedia";
 import "./contact.css";
 import "../App.css";
 import backgroundimage from "../images/contact.jpg";
@@ -6,8 +7,13 @@ import backgroundimage from "../images/contact.jpg";
 const contact = props => {
   return (
     <div className="contact view">
-      <img className="background-image" src={backgroundimage} />
-      <h1 className="page-title">Skills</h1>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${backgroundimage})` }}
+      >
+        <SocialMedia />
+        <h1 className="page-title">Skills</h1>
+      </div>
     </div>
   );
 };
