66051dd9e0f156767c450a0970192c0f41cf8ba3
Jordan <jspeed8th@gmail.com>
2019-01-25 13:47:47 -0500

    Updates

---
+++ precache-manifest.47f2bd82efe65960c87b90acb1555531.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "884a75835e668e1f5b19a985d4e14871",
+    "url": "/portfolio/static/media/contact.884a7583.jpg"
+  },
+  {
+    "revision": "39c70badb851d84f5305",
+    "url": "/portfolio/static/css/main.1d58cee8.chunk.css"
+  },
+  {
+    "revision": "c9e84c8aea05f1c3f5c1",
+    "url": "/portfolio/static/js/1.c9e84c8a.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "eb36817db1c652e7d88381c5de02e83c",
+    "url": "/portfolio/static/media/home.eb36817d.jpg"
+  },
+  {
+    "revision": "66657f5dc34703daceb62cb80cf2f7d4",
+    "url": "/portfolio/static/media/react-logo.66657f5d.png"
+  },
+  {
+    "revision": "fb3a4c3161d3e7c593c2f55cd87326ba",
+    "url": "/portfolio/static/media/about.fb3a4c31.jpg"
+  },
+  {
+    "revision": "31a3f133ae66e00ab26970763f065768",
+    "url": "/portfolio/static/media/projects.31a3f133.jpg"
+  },
+  {
+    "revision": "b6dc5042b75bcc556ceaf4c3451c09ac",
+    "url": "/portfolio/static/media/jordansBakery.b6dc5042.mov"
+  },
+  {
+    "revision": "398ad81ca6baa72c28248ee2e364bbec",
+    "url": "/portfolio/static/media/pokemon.398ad81c.mov"
+  },
+  {
+    "revision": "72de4564bc2b3a27b5f3cc9bb5264295",
+    "url": "/portfolio/static/media/tastebuds.72de4564.mov"
+  },
+  {
+    "revision": "39c70badb851d84f5305",
+    "url": "/portfolio/static/js/main.39c70bad.chunk.js"
+  },
+  {
+    "revision": "8494c5427984d4c8dfdb77f218cc83a2",
+    "url": "/portfolio/static/media/resume-two-logo-LEG.8494c542.png"
+  },
+  {
+    "revision": "0966a3d8d8bab50a7ff1520620ce3a37",
+    "url": "/portfolio/static/media/css-logo.0966a3d8.jpg"
+  },
+  {
+    "revision": "2c321d3d1512aa3bd347a24999b2497e",
+    "url": "/portfolio/static/media/html-logo.2c321d3d.jpg"
+  },
+  {
+    "revision": "9020ffe91dd490c1e4a52fd8805a14f7",
+    "url": "/portfolio/static/media/javascript-logo.9020ffe9.jpg"
+  },
+  {
+    "revision": "344707411f488900e85e8bf04ac616bc",
+    "url": "/portfolio/static/media/rails-logo.34470741.png"
+  },
+  {
+    "revision": "dcb1b92c98fcfc68602381dd13834861",
+    "url": "/portfolio/static/media/react-logo.dcb1b92c.jpg"
+  },
+  {
+    "revision": "369a993f4d6b4c71d8f82b5ae3f7d996",
+    "url": "/portfolio/static/media/resume-logo.369a993f.jpg"
+  },
+  {
+    "revision": "b43c43bb82c0a373f6a891e2a25dffe2",
+    "url": "/portfolio/static/media/ruby-logo.b43c43bb.png"
+  },
+  {
+    "revision": "493c53b4123d3eaddf8d363d0064d922",
+    "url": "/portfolio/static/media/sass-logo.493c53b4.jpg"
+  },
+  {
+    "revision": "f62d098f29fdd3c961e0858873aaf59d",
+    "url": "/portfolio/index.html"
+  }
+];
