f84128c5a143695e316598debb2321054474eb1f
Jordan <jspeed8th@gmail.com>
2019-03-07 18:02:30 -0500

    yup

--- src/components/home/MyName/MyName.js
+++ src/components/home/MyName/MyName.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './MyName.css';
 
-const MyName = () => {
+const MyName = ({ route }) => {
+	const style = {
+		display: 'block'
+	};
+	route === 'Home' ? style.transition = '1s' : style.display = 'none';
 	return (
-		<div className="home-title">
+		<div className="home-title" style = {style}>
           <h1>Jordan Speed</h1>
           <p>full stack web developer</p>
         </div>
