2c4ba722ed7bdaf514c4ec020bcbec0a20e6fa1d
Jordan <jspeed8th@gmail.com>
2019-03-07 23:07:59 -0500

    nothing

--- src/components/home/MyName/MyName.js
+++ src/components/home/MyName/MyName.js
@@ -3,14 +3,14 @@ import './MyName.css';
 
 const MyName = ({ route }) => {
 	const style = {
-		display: 'block'
+		display: 'inline-block'
 	};
-	route === 'Home' ? style.animationName = 'slideup' : style.display = 'none';
+	route === 'Home' ? style.animationName = 'appear-and-slide-up' : style.display = 'none';
 	return (
-		<div className="home-title" style = {style}>
-          <h1>Jordan Speed</h1>
-          <p>full stack web developer</p>
-        </div>
+		<div className="home-title slide-duration" style = {style}>
+      <h1>Jordan Speed</h1>
+      <p>full stack web developer</p>
+    </div>
 	)
 };
 
