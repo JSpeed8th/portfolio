a761ee0fd541e5c11a63e0d925a53dcfd7ac1f9f
Jordan <jspeed8th@gmail.com>
2019-03-08 01:20:57 -0500

    nothing

--- src/components/about/Paragraph.js
+++ src/components/about/Paragraph.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Paragraph = () => {
 	return (
 		<p>
-			Hi. I'm Jordan, a software developer from Philadephia, PA. 
-			I'm passionate about building software that impacts people's lives. 
-			I love problem solving and believe that every problem has a solution! 
-			My graphic arts background influences the cool designs you see today. 
+			Hi. I'm Jordan, a software developer from Philadephia, PA.
+			I'm passionate about building software that impacts people's lives.
+			I love problem solving and believe that every problem has a solution!
+			My graphic arts background influences the cool designs you see today.
 			I love to travel, hence the beautiful photgraphs taken by my fiance during our time in Iceland. 
 			I'm a naturists through and through and try to live as organic and wastless as possible.
         </p>
