75296d9fc2de29192549a41cb52311955af4d408
Jordan <jspeed8th@gmail.com>
2019-03-14 20:15:36 -0400

    Corrected typo in about me section

--- src/components/about/Paragraph.js
+++ src/components/about/Paragraph.js
@@ -7,7 +7,7 @@ const Paragraph = () => {
 			I'm passionate about building software that impacts people's lives.
 			I love problem solving and believe that every problem has a solution!
 			My graphic arts background influences the cool designs you see today.
-			I love to travel, hence the beautiful photgraphs taken by my fiance during our time in Iceland. 
+			I love to travel, hence the beautiful photographs taken by my fiance during our time in Iceland. 
 			I'm a naturists through and through and try to live as organic and wastless as possible.
         </p>
 		)
